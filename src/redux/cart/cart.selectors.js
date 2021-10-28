2d383eeb253f597398cfa994f1e0839413d9df35
Rohit Kumar <shutupRohit@gmail.com>
2021-10-29 00:01:45 +0530

    using reselct to memoiz

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,13 @@
+import { createSelector } from "reselect";
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((prevValue, cartItem) => prevValue+cartItem.quantity, 0)
+)
