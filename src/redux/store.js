5ee93bd41b0c9b18bf5241ede7676662f5830d26
Rohit Kumar <shutupRohit@gmail.com>
2022-02-11 14:50:19 +0530

    logger middleware config

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,11 @@ import rootReducer  from "./root-reducer";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [logger, sagaMiddleware];  
+const middlewares = [sagaMiddleware];  
+
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
